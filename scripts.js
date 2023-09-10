e5318b075e549d959ac0315d7c83109b5574198d
Sophia <sophiamperez@hotmail.com>
2023-09-10 20:37:08 -0300

    [add] initial commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function changeColor() {
+  document.getElementById("texto").style.color = "blue";
+}
+
+function baseColor() {
+  document.getElementById("texto").style.color = "black";
+}
+
+function changeColor2() {
+  document.getElementById("texto2").style.color = "blue";
+}
+
+function baseColor2() {
+  document.getElementById("texto2").style.color = "black";
+}
+
+function changeColor3() {
+  document.getElementById("texto3").style.color = "blue";
+}
+
+function baseColor3() {
+  document.getElementById("texto3").style.color = "black";
+}
